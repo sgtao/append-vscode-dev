9707157ae1b43e1e5cb72296cec0487d68c67abf
sgtao <sg.tao.so@gmail.com>
2022-12-04 17:30:22 +0900

    update docs for append actions of illegal input

--- docs/scripts/convertToVScodeDev.js
+++ docs/scripts/convertToVScodeDev.js
@@ -23,29 +23,21 @@ const convertToVScodeDev = (() => {
         keys.forEach((key) => githubInfo[key] = '');
     };
     const convert = () => {
-        init_githubInfo();
         console.log(inputUserValue.value);
-        try {
-            let inputProjectURL = new URL(inputUserValue.value);
-            console.log(inputProjectURL);
-            let projectHostname = inputProjectURL.hostname;
-            if (projectHostname.indexOf('https://github.com') === -1) {
-                devItem.path = inputProjectURL.pathname;
-                console.log(devItem);
-                get_githubapi(inputUserValue.value);
-                let vscodeURL = devItem.url + "github.com" + devItem.path;
-                resultTextArea.innerHTML = `<p>${devItem.name}</p><p>path : ${devItem.path};</p>`;
-                resultTextArea.innerHTML += `<p>URL : <a href="${vscodeURL}" target="_blank">${vscodeURL}</a></p>`;
-            } else {
-                resultTextArea.textContent = "githubのレポジトリURLを入力してください";
-            }
-        } catch (e) {
-            console.log(e);
-            if (e instanceof TypeError) {
-                resultTextArea.textContent = "レポジトリのURLを入力してください";
-            } else {
-                resultTextArea.textContent = e;
-            }
+        init_githubInfo();
+        resultTextArea.textContent = "";
+        let inputProjectURL = new URL(inputUserValue.value);
+        console.log(inputProjectURL);
+        console.log(inputProjectURL.origin);
+        if (inputProjectURL.origin === 'https://github.com') {
+            devItem.path = inputProjectURL.pathname;
+            console.log(devItem);
+            get_githubapi(inputUserValue.value);
+            let vscodeURL = devItem.url + "github.com" + devItem.path;
+            resultTextArea.innerHTML = `<p>${devItem.name}</p><p>path : ${devItem.path};</p>`;
+            resultTextArea.innerHTML += `<p>URL : <a href="${vscodeURL}" target="_blank">${vscodeURL}</a></p>`;
+        } else {
+            resultTextArea.textContent = "githubのレポジトリURLを入力してください";
         }
     };
     const get_githubapi = (inputUserValue) => {
@@ -78,10 +70,27 @@ const convertToVScodeDev = (() => {
                 githubInfo.avatar_url = res.data.avatar_url;
             })
             // catchでエラー時の挙動を定義
-            .catch(err => {
-                console.log("err:", err);
+            .catch(error => {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                    if (error.response.status === 404) {
+                        resultTextArea.textContent = "該当するユーザーはいません";
+                    }
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                }
+                console.log(error.config);
             });
-
     }
     return {
         convert
