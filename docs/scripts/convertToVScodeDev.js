42a429661637053e330d89425f4aab3026529898
sgtao <sg.tao.so@gmail.com>
2022-12-04 17:10:09 +0900

    update docs for get github user's info with axios

--- docs/scripts/convertToVScodeDev.js
+++ docs/scripts/convertToVScodeDev.js
@@ -10,8 +10,20 @@ const convertToVScodeDev = (() => {
         path : "/",
         label: "Visual Studio Code",
     }
-    console.log(inputUserValue.value);
+    const githubInfo = {
+        user: '',
+        project: '',
+        email: '',
+        avatar_url: '',
+        name: '',
+        repos_url: '',
+    };
+    const init_githubInfo = () => {
+        let keys = Object.keys(githubInfo)
+        keys.forEach((key) => githubInfo[key] = '');
+    };
     const convert = () => {
+        init_githubInfo();
         console.log(inputUserValue.value);
         try {
             let inputProjectURL = new URL(inputUserValue.value);
@@ -20,6 +32,7 @@ const convertToVScodeDev = (() => {
             if (projectHostname.indexOf('https://github.com') === -1) {
                 devItem.path = inputProjectURL.pathname;
                 console.log(devItem);
+                get_githubapi(inputUserValue.value);
                 let vscodeURL = devItem.url + "github.com" + devItem.path;
                 resultTextArea.innerHTML = `<p>${devItem.name}</p><p>path : ${devItem.path};</p>`;
                 resultTextArea.innerHTML += `<p>URL : <a href="${vscodeURL}" target="_blank">${vscodeURL}</a></p>`;
@@ -34,6 +47,41 @@ const convertToVScodeDev = (() => {
                 resultTextArea.textContent = e;
             }
         }
+    };
+    const get_githubapi = (inputUserValue) => {
+        console.log('inputUserValue: ' + inputUserValue);
+        inputUserValue.split('/').map((path, index)=>{
+            switch(index) {
+                case 2:
+                    console.log(path);
+                    break;
+                case 3:
+                    console.log(path);
+                    githubInfo.user = path;
+                    break;
+                case 4:
+                    console.log(path);
+                    githubInfo.project = path;
+                    break;
+            }
+        });
+        let githubaUserURL = 'https://api.github.com/users/' + githubInfo.user;
+        // GETリクエスト（通信）
+        axios.get(githubaUserURL)
+
+            // thenで成功した場合の処理
+            .then((res) => {
+                console.log(res.data);
+                githubInfo.email = res.data.email;
+                githubInfo.name  = res.data.name;
+                githubInfo.repos_url  = res.data.repos_url;
+                githubInfo.avatar_url = res.data.avatar_url;
+            })
+            // catchでエラー時の挙動を定義
+            .catch(err => {
+                console.log("err:", err);
+            });
+
     }
     return {
         convert
